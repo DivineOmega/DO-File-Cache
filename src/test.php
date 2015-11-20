e6343b20421dbbb9a0b9976c5c9fbab748c177c7
James Brooks <james@alt-three.com>
2015-11-20 13:37:28 -0500

    Applied fixes from StyleCI

--- src/test.php
+++ src/test.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once "RWFileCache.php";
+require_once 'RWFileCache.php';
 
 $cache1 = new \rapidweb\RWFileCache\RWFileCache();
 
-$cache1->changeConfig(array("cacheDirectory" => "/tmp/rwFileCacheStorage/"));
+$cache1->changeConfig(['cacheDirectory' => '/tmp/rwFileCacheStorage/']);
 
-$cache1->set('first.test.stringContent', "Mary had a little lamb.", strtotime('+ 1 day'));
+$cache1->set('first.test.stringContent', 'Mary had a little lamb.', strtotime('+ 1 day'));
 
 $var = $cache1->get('first.test.stringContent');
 
