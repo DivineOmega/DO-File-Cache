b6245446c23598963288d9f5b64c2444e937f830
Jordan Hall <jordan.hall@rapidweb.biz>
2017-07-07 09:43:48 +0100

    Add basic functional tests

---
+++ tests/Functional/CacheStorageAndRetrievalTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use RapidWeb\uxdm\Objects\Sources\CSVSource;
+
+final class CacheStorageAndRetrievalTest extends TestCase
+{
+    private $cache = null;
+
+    public function setUp()
+    {
+        $this->cache = new \rapidweb\RWFileCache\RWFileCache();
+        $this->cache->changeConfig(['cacheDirectory' => '/tmp/rwFileCacheTesting/']);
+    }
+
+    public function testBasicString()
+    {
+        $stored = 'Mary had a little lamb.';
+
+        $key = __FUNCTION__;
+        $this->cache->set($key, $stored, strtotime('+ 1 day'));
+
+        $retrieved = $this->cache->get($key);
+
+        $this->assertEquals($stored, $retrieved);
+    }
+
+    public function testEmptyArray()
+    {
+        $stored = [];
+
+        $key = __FUNCTION__;
+        $this->cache->set($key, $stored, strtotime('+ 1 day'));
+
+        $retrieved = $this->cache->get($key);
+
+        $this->assertEquals($stored, $retrieved);
+    }
+
+    public function testNumericZero()
+    {
+        $stored = 0;
+
+        $key = __FUNCTION__;
+        $this->cache->set($key, $stored, strtotime('+ 1 day'));
+
+        $retrieved = $this->cache->get($key);
+
+        $this->assertEquals($stored, $retrieved);
+    }
+
+    public function testBooleanFalse()
+    {
+        $stored = false;
+
+        $key = __FUNCTION__;
+        $this->cache->set($key, $stored, strtotime('+ 1 day'));
+
+        $retrieved = $this->cache->get($key);
+
+        $this->assertEquals($stored, $retrieved);
+    }
+
+    public function testBooleanTrue()
+    {
+        $stored = false;
+
+        $key = __FUNCTION__;
+        $this->cache->set($key, $stored, strtotime('+ 1 day'));
+
+        $retrieved = $this->cache->get($key);
+
+        $this->assertEquals($stored, $retrieved);
+    }
+}
