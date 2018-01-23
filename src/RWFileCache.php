bf0f73ac0e8ae44c31d78c762e8c20a9ebcf444b
Jordan Hall <jordan.hall@rapidweb.biz>
2018-01-23 16:57:13 +0000

    Set open permissions on created directories

--- src/RWFileCache.php
+++ src/RWFileCache.php
@@ -319,6 +319,10 @@ class RWFileCache
             }
         }
 
+        if (!is_readable($directoryToCreate) || !is_writable($directoryToCreate)) {
+            chmod($directoryToCreate, 0777);
+        }
+
         $filePath = $this->config['cacheDirectory'].$key.'.'.$this->config['fileExtension'];
 
         return $filePath;
